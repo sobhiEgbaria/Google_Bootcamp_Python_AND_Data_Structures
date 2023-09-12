bcf92b4edd843b5fca6b0038d8ced0aba93b0a0c
sobhiEgbaria <sobhim686@gmail.com>
2023-09-12 18:27:29 +0300

    setion 8 linkedList

---
+++ DS/day_0/section_3_big(o).py
@@ -0,0 +1,15 @@
+"""
+big o notation and time complexity:
+"""
+"""
+O(1) < O(log(n)) < O(n) < O(n*log(n)) < O(n^2) < O(2^n) < O(n!).
+
+O(1) constant time
+O(log(n))
+O(n) : linear time complexity
+O(n*log(n))
+O(n^2)
+<O(2^n)
+<O(n!).
+
+"""
--- DS/day_3/DS_day_3.py
+++ DS/day_3/DS_day_3.py
@@ -1,18 +1,18 @@
-# Question 1
-class PersonNode:
-    def __init__(self, name, age, next_node=None):
-        self.name = name
-        self.age = age
-        self.next_node = next_node
-        pass
+# #  Question 1
+# class PersonNode:
+#     def __init__(self, name, age, next_node=None):
+#         self.name = name
+#         self.age = age
+#         self.next_node = next_node
+#         pass
 
-    def __repr__(self):
-        res = (self.name, self.age)
-        return repr(res)
+#     def __repr__(self):
+#         res = (self.name, self.age)
+#         return repr(res)
 
 
-p1 = PersonNode("sobhi", "18")
-print(p1)
+# p1 = PersonNode("sobhi", "18")
+# print(p1)
 
 
 # # # Question 2
@@ -56,7 +56,7 @@ print(p1)
 
 #     def copy_list(self):
 #         """
-#         Create and return a deep copy of the list. Do not use the copy module of python.
+#   Create and return a deep copy of the list. Do not use the copy module of python.
 #         """
 #         pass
 
@@ -64,156 +64,156 @@ print(p1)
 # p1 = PersonList.add(("gogo"), ("18"))
 # print(p1)
 
-# #     # Question 4
-# #     def print_line(self):
-# #         """
-# #         Print the queue and its length.
-# #         """
-# #         pass
+#   #  # Question 4
+#     def print_line(self):
+#         """
+#         Print the queue and its length.
+#         """
+#         pass
 
-# #     def is_in_line(self, name):
-# #         """
-# #         Check if a person with the given name is in the queue.
-# #         """
-# #         pass
+#     def is_in_line(self, name):
+#         """
+#         Check if a person with the given name is in the queue.
+#         """
+#         pass
 
-# #     def add_israeli(self, name, age):
-# #         """
-# #         Add a new Israeli person to the queue.
-# #         """
-# #         pass
+#     def add_israeli(self, name, age):
+#         """
+#         Add a new Israeli person to the queue.
+#         """
+#         pass
 
-# #     def remove(self, name):
-# #         """
-# #         Remove a person with the given name from the queue.
-# #         """
-# #         pass
+#     def remove(self, name):
+#         """
+#         Remove a person with the given name from the queue.
+#         """
+#         pass
 
-# #     def add_VIP(self, name, age):
-# #         """
-# #         Add a VIP person to the top of the queue.
-# #         """
-# #         pass
+#     def add_VIP(self, name, age):
+#         """
+#         Add a VIP person to the top of the queue.
+#         """
+#         pass
 
-# #     def reverse_list(self):
-# #         """
-# #         Reverse the order of the list.
-# #         """
-# #         pass
+#     def reverse_list(self):
+#         """
+#         Reverse the order of the list.
+#         """
+#         pass
 
-# #     # Question 5
-# #     def contains_loop(self):
-# #         """
-# #         Check if the list contains a loop.
-# #         """
-# #         pass
-
-
-# # # Question 3
-# # def main():
-# #     """
-# #     Main function to demonstrate the usage of the PersonList class.
-# #     """
-# #     pass
+#     # Question 5
+#     def contains_loop(self):
+#         """
+#         Check if the list contains a loop.
+#         """
+#         pass
 
 
-# # # Question 6
-# # def is_tail(head_x, head_y):
-# #     """
-# #     Check if head_y is a tail of head_x.
-# #     """
-# #     pass
+# # Question 3
+# def main():
+#     """
+#     Main function to demonstrate the usage of the PersonList class.
+#     """
+#     pass
 
 
-# # class Node:
-# #     def __init__(self, data=None):
-# #         """
-# #         Initialize a node with data and a reference to the next node.
-# #         """
-# #         self.data = data
-# #         self.next = None
+# # Question 6
+# def is_tail(head_x, head_y):
+#     """
+#     Check if head_y is a tail of head_x.
+#     """
+#     pass
 
 
-# # # Question 7
-# # class LinkedList:
-# #     def __init__(self):
-# #         """
-# #         Initialize an empty linked list.
-# #         """
-# #         pass
-
-# #     def insert_last(self, x):
-# #         """
-# #         Insert a node with the given data at the end of the list.
-# #         """
-# #         pass
-
-# #     def __repr__(self):
-# #         """
-# #         Return a string representation of the entire list.
-# #         """
-# #         pass
-
-# #     def insert(self, i, x):
-# #         """
-# #         Insert a node with the given data at the specified index.
-# #         """
-# #         pass
-
-# # def remove(self, i):
-# #     if self.head == None:
-# #         return None
-# #     if self.head.get_next() == None:
-# #         self.head = None
-# #         return None
-# #     prev = self.head
-# #     current = self.head.get_next()
-# #     while current.get_next() is not i:
-# #         current = current.get_next()
-# #         prev.set_next = None
-# #         return current.get_data()
-# #         pass
-
-# #     def reverse(self):
-# #         """
-# #         Reverse the order of the list.
-# #         """
-# #         pass
+# class Node:
+#     def __init__(self, data=None):
+#         """
+#         Initialize a node with data and a reference to the next node.
+#         """
+#         self.data = data
+#         self.next = None
 
 
-# # class RecursiveLinkedList:
-# #     def __init__(self):
-# #         """
-# #         Initialize an empty linked list.
-# #         """
-# #         pass
+# # Question 7
+# class LinkedList:
+#     def __init__(self):
+#         """
+#         Initialize an empty linked list.
+#         """
+#         pass
 
-# #     def insert_last(self, x):
-# #         """
-# #         Insert a node with the given data at the end of the list.
-# #         """
-# #         pass
+#     def insert_last(self, x):
+#         """
+#         Insert a node with the given data at the end of the list.
+#         """
+#         pass
 
-# #     def __repr__(self):
-# #         """
-# #         Return a string representation of the entire list.
-# #         """
-# #         pass
+#     def __repr__(self):
+#         """
+#         Return a string representation of the entire list.
+#         """
+#         pass
 
-# #     def insert(self, i, x):
-# #         """
-# #         Insert a node with the given data at the specified index.
-# #         """
-# #         pass
+#     def insert(self, i, x):
+#         """
+#         Insert a node with the given data at the specified index.
+#         """
+#         pass
 
-# #     def remove(self, i):
-# #         """
-# #         Remove and return the data of the node at the specified index.
-# #         """
-# #         pass
+# def remove(self, i):
+#     if self.head == None:
+#         return None
+#     if self.head.get_next() == None:
+#         self.head = None
+#         return None
+#     prev = self.head
+#     current = self.head.get_next()
+#     while current.get_next() is not i:
+#         current = current.get_next()
+#         prev.set_next = None
+#         return current.get_data()
+#         pass
 
-# #     def reverse(self):
-# #         """
-# #         Reverse the order of the list.
-# #         """
-# #         pass
+#     def reverse(self):
+#         """
+#         Reverse the order of the list.
+#         """
+#         pass
+
+
+# class RecursiveLinkedList:
+#     def __init__(self):
+#         """
+#         Initialize an empty linked list.
+#         """
+#         pass
+
+#     def insert_last(self, x):
+#         """
+#         Insert a node with the given data at the end of the list.
+#         """
+#         pass
+
+#     def __repr__(self):
+#         """
+#         Return a string representation of the entire list.
+#         """
+#         pass
+
+#     def insert(self, i, x):
+#         """
+#         Insert a node with the given data at the specified index.
+#         """
+#         pass
+
+#     def remove(self, i):
+#         """
+#         Remove and return the data of the node at the specified index.
+#         """
+#         pass
+
+#     def reverse(self):
+#         """
+#         Reverse the order of the list.
+#         """
+#         pass
